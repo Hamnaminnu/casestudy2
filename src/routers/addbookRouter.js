d288876ab71ac3e5d2e5ffd10dcc39ae5cd29dd4
Hamnaminnu <67734377+Hamnaminnu@users.noreply.github.com>
2021-01-25 09:35:44 +0530

    second commit

--- src/routers/addbookRouter.js
+++ src/routers/addbookRouter.js
@@ -2,8 +2,12 @@ const express = require('express');
 const addbookRouter = express.Router();
 const bookdata = require('../model/bookdata');
 const addauthorRouter = require('./addauthorRouter');
+var multer  = require('multer');
+const upload = require('express-fileupload');
+const fs = require("fs");
+addbookRouter.use(upload());
 addbookRouter.use(express.static('./public'));
-addauthorRouter.use(express.urlencoded({extended:true}));
+addbookRouter.use(express.urlencoded({extended:true}));
 addbookRouter.get('/',function(req,res){
     res.render("addbook",
     {
@@ -17,8 +21,21 @@ addbookRouter.post('/done',function(req,res){
         author: req.body.author,
         genre: req.body.genre,
         more: req.body.more,
-        img: req.body.img
+        img: req.files.img1.name
     }
+    
+    var file = req.files.img1;
+    var filename = req.files.img1.name;
+
+    
+    file.mv("./public/images/"+filename,function(err){
+        if(err){
+            res.send(err);
+        }
+    });
+
+
+
     var book = bookdata(item);
     book.save();
     bookdata.find()
